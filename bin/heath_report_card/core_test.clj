db4688a50920a2c930e91e5338da9a4b1f82eb69
Adrian Osullivan <adrian.osullivan@db.com>
2014-07-23 11:49:27 -0400

    Refactoring and adding tests

--- bin/heath_report_card/core_test.clj
+++ bin/heath_report_card/core_test.clj
@@ -13,72 +13,46 @@
             [clojure.tools.logging :as log]))
 
 
-(defn capture-console [msg f] 
-  (def bstream (ByteArrayOutputStream.))
-  (def pstream (PrintStream. bstream))
-  (def out (System/out))
-  (log/info "Starting:" msg)
-  (System/setOut pstream)
-  (eval f)
-  (System/setOut out)
-  (log/info "Finished:" msg)
-  (ByteArrayInputStream. (.toByteArray bstream)))
-
-;(def src "E:\\workspace-healthcheck\\CDR\\CDR\\JavaDev")
-(def src "E:\\workspace-sdlc\\javancss\\src\\main\\java")
-
-
-;Run CPD
-(do
-  (System/setProperty (CPDCommandLineInterface/NO_EXIT_AFTER_RUN) "true" )
-  (def cpd-seq (xml-seq (xml/parse (capture-console "CPD" '(CPD/main (into-array ["--files" src "--minimum-tokens" "80" "--format" "xml"]))))))
-  (def duplicated-lines (apply + (for [node cpd-seq :when (= :duplication (:tag node))] (read-string (:lines (:attrs node) ))))))
-
-
-;Run NCSS
-(do
-  (Locale/setDefault (Locale/US))
-  (defn to-int [seq] (read-string (clojure.string/replace (first seq) #"," "")))
-  (def ncss-zip (zip/xml-zip (xml/parse (capture-console "NCSS" '(Javancss. (into-array ["-xml" "-all" src]) Main/S_RCS_HEADER)))))
-  (def ccn (to-int (zf/xml-> ncss-zip :functions :function_averages :ccn  zf/text )))
-  (def ncss (to-int (zf/xml-> ncss-zip :functions :ncss zf/text))))
-
-;Run PMD
-(do    
-  (def pmd-seq (xml-seq (xml/parse (capture-console "PMD" '(PMD/main (into-array ["-R" "./rulesets/ruleset.xml" "-f" "xml" "-d" src])))  )))    
+  ;(def src "E:\\workspace-healthcheck\\CDR\\CDR\\JavaDev")
+  (def src "E:\\workspace-sdlc\\javancss\\src\\main\\java")
   
-  (defn is-included? [node rule] (and (= :violation (:tag node)) (= rule (:rule (:attrs node)))))
-  (defn length [node] (- (read-string (:endline (:attrs node))) (read-string (:beginline (:attrs node)))))
-  (defn loop-lengths [rule] (for [node pmd-seq :when (is-included? node rule) ]  (length node)))
+  (def src-dup "E:\\workspace-sdlc\\test\\src\\main\\java\\duplication")
+  (def src-dupmore "E:\\workspace-sdlc\\test\\src\\main\\java\\duplicationx3")
+  (def src-nodup "E:\\workspace-sdlc\\test\\src\\main\\java\\noduplication")
+
+  (deftest cpd-duplicated-lines-test
+    (testing "10 line duplicate, includes whitespace and braces"
+     (is (= 10 (dup-lines-count src-dup)))))
+
+   (deftest cpd-duplicated-morelines-test
+    (testing "10 line duplicate in 3 files, includes whitespace and braces"
+     (is (= 10 (dup-lines-count src-dupmore)))))
+
+  (deftest cpd-duplicated-lines-test
+    (testing "Zero lines duplication"
+     (is (= 0 (dup-lines-count src-nodup)))))
+    
+; src not found
+
   
-  (def all-methods (loop-lengths "ExcessiveMethodLength"))
-  (def all-classes (loop-lengths "ExcessiveClassLength"))
-  
-  (def average-method-length (double (/ (apply + all-methods) (count all-methods) )))
-  (def average-class-length (double (/ (apply + all-classes) (count all-classes) ))))
+;  (deftest locale-US
+;   (testing "Locale should be US"
+;     (is (= (= (Locale/getDefault) (Locale/US))))))
+
+(dup-lines-count src-nodup)
+
+(run-tests)
 
 
-  (pprint {:non-comment-lines ncss
-           :cyclomatic-complexity ccn            
-           :average-method-length average-method-length
-           :average-class-length average-class-length
-           :duplicated-lines duplicated-lines})
-  
-  
 ;TODO 
 ; CPD - count instances, if 3, then should add this total again
+; use let and letfn to descrease mem
 ; save the xml for reference/debugging
 ; ccn / ncss to double
-; test calculations on some real code
+; when does it actually exec the commandline, cos everything is a def
+; unit test calculations on some real code
 ; capture console to XML, not to byte array
 ; multithread
 ; package
 ; scm
-
-(deftest locale-US
-  (testing "Locale should be US"
-    (is (= (= (Locale/getDefault) (Locale/US))))))
-
-
-(run-tests)
 
